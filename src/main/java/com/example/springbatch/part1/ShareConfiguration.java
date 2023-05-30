76487dd9f171e3b0d0035b87eb116ef9db0145a5
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-05-30 22:11:51 +0900

    4.데이터 공유

--- src/main/java/com/example/springbatch/part1/ShareConfiguration.java
+++ src/main/java/com/example/springbatch/part1/ShareConfiguration.java
@@ -1,21 +1,20 @@
-# 4. 데이터공유
+package com.example.springbatch.part1;
 
-## ShareConfiguration 클래스 생성
-```java
-@Slf4j
-@Configuration
-public class ShareConfiguration {
-	
-}
-```
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
----  
-
-<br>
-<br>  
-
-## shareJob과 shareStep1, shareStep2 생성
-```java
 @Slf4j
 @Configuration
 public class ShareConfiguration {
@@ -83,51 +82,3 @@ public class ShareConfiguration {
 								 .build();
 	}
 }
-
-```
-
-<br>  
-
-> 위 코드를 실행하면 아래 와 같은 결과가 나온다.
-```text
-===================1====================
-2023-05-30 21:59:44.929  INFO 11596 --- [           main] c.e.s.part1.ShareConfiguration           : jobName : shareJob, stepName : shareStep, parameter 8
-2023-05-30 21:59:44.975  INFO 11596 --- [           main] o.s.batch.core.step.AbstractStep         : Step: [shareStep] executed in 132ms
-2023-05-30 21:59:45.370  INFO 11596 --- [           main] o.s.batch.core.job.SimpleStepHandler     : Executing step: [shareStep2]
-==================2=====================
-2023-05-30 21:59:45.444  INFO 11596 --- [           main] c.e.s.part1.ShareConfiguration           : jobName : shareJob, jobKey : job execution context, stepKey : emptyStepKey
-2023-05-30 21:59:45.492  INFO 11596 --- [           main] o.s.batch.core.step.AbstractStep         : Step: [shareStep2] executed in 122ms
-2023-05-30 21:59:45.588  INFO 11596 --- [           main] o.s.b.c.l.support.SimpleJobLauncher      : Job: [SimpleJob: [name=shareJob]] completed with the following parameters: [{run.id=8}] and the following status: [COMPLETED] in 809ms
-```
-
-
-<br>  
-
-> step간에는 데이터 공유가 가능하다 하지만 주의할 점은 shareStep1 에서`stepExecutionContext.put("stepKey", "step execution context");` 는 shareStep2에서 공유가 안되고  `jobExecutionContext.putString("jobKey", "job execution context");` 는 공유가 된다는 것이다.
-
----
-
-<br>
-<br>
-
-
-## 비고
-> 현재는 HelloConfiguration와 ShareConfiguration클래스에 있는 job이 동시에 실행한다. 아래와 같은 설정을 해줘야 하나만 작동한다.
-```yaml
-server:
-  port: 8090
-
-spring:
-  batch:
-    job:
-      names: ${job.name:NONE}    # 주석해제
-    jdbc:
-      initialize-schema: never
-  datasource:
-    url: jdbc:mysql://localhost:3306/spring_batch?characterEncoding=UTF-8&serverTimezone=UTC
-    driver-class-name: com.mysql.cj.jdbc.Driver
-    username: root
-    password: 1234
-```
-
-![img_3.png](img_3.png)
